d235aa4eaa1b6e61c95f2161a870934e4462067e
arturomf94 <arturomf94@gmail.com>
2019-06-22 16:29:22 -0500

    make topology dynamic

--- docs/examples/tutorials/topology/test_ring.py
+++ docs/examples/tutorials/topology/test_ring.py
@@ -1,6 +1,7 @@
 # Import modules
 import numpy as np
 import numpy.ma as ma
+import math
 
 # Import sphere function as objective function
 from pyswarms.utils.functions.single_obj import sphere as f
@@ -9,7 +10,7 @@ from pyswarms.utils.functions.single_obj import sphere as f
 import pyswarms.backend as P
 from pyswarms.backend.topology import AdaptiveRing
 
-N = 50
+N = 100
 my_topology = AdaptiveRing() # The Topology Class
 my_options = {'c1': 0.6, 'c2': 0.3, 'w': 0.4,
                 'feasibility': np.zeros(N, dtype = bool),
@@ -39,7 +40,9 @@ def con2(x):
     feasible = np.asarray([var[1] for var in feasible])
     return feasible
 
-iterations = 100 # Set 100 iterations
+iterations = 500 # Set 100 iterations
+k_delta = math.ceil( (N - 1) / iterations) # additional neighbors each gen
+k = k_delta
 # Check feasibility and update personal best only if feasible
 my_swarm.options['feasibility'] = all_constraints(my_swarm.position)
 my_swarm.current_cost = f(my_swarm.position)
@@ -59,9 +62,14 @@ for i in range(iterations):
         my_swarm.best_cost = min(x for x in my_swarm.pbest_cost if x is not None)
         min_pos_id = np.where(my_swarm.pbest_cost == my_swarm.best_cost)[0][0]
         my_swarm.options['best_position'] = my_swarm.pbest_pos[min_pos_id]
-    my_swarm.best_pos = my_topology.compute_gbest(my_swarm, p = 2, k = 5)
+
+    if k < N - 1:
+        my_swarm.best_pos = my_topology.compute_gbest(my_swarm, p = 2, k = k)
+    else:
+        my_swarm.best_pos = my_topology.compute_gbest(my_swarm, p = 2, k = (N - 1))
+    k += k_delta
     # Let's print our output
-    if i%20==0:
+    if i%50==0:
         print('Iteration: {} | my_swarm.best_cost: {:.4f}'.format(i+1, my_swarm.best_cost))
 
     # Part 3: Update position and velocity matrices
